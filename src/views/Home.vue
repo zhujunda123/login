55388eee0241cc0c6d70b45c5fd49f699d694608
zhujunda1991 <327812345@qq.com>
2020-01-01 10:13:58 +0800

    diyici

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div>213234324</div>
   <div>这是我们的sss</div>
     <div>这是我们的1111</div>
   <div>这是我们的222主页</div>
     <el-button @click="ss">登出</el-button>
+       <div>222222222</div>
   </div>
+   
+
 </template>
 
 <script>
