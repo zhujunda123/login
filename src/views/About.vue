c1cb850daf42ec426a2b8996cad512cca133ae18
zhujunda1991 <327812345@qq.com>
2020-01-02 23:00:44 +0800

    sss

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+         <div>222222222</div>
+        <div>这是我们的333</div>
+        <div>这是我们的222</div>
+          <div>这是我们的333</div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <div>213234324</div>
-  <div>这是我们的sss</div>
-    <div>这是我们的1111</div>
-  <div>这是我们的222主页</div>
+
     <el-button @click="ss">登出</el-button>
-       <div>222222222</div>
-        <div>这是我们的333</div>
-        <div>这是我们的222</div>
-          <div>这是我们的333</div>
+  
   </div>
    
 
